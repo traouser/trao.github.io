f0a1149065e43d08517f3ea52014dd6c1e3a21fc
radioshiny <shinykim@kasi.re.kr>
2023-08-30 15:38:29 +0900

    initial update

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TRAO'
+copyright = '2023, Shinyoung Kim'
+author = 'Shinyoung Kim'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # Sphinx's own extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # Our custom extension, only meant for Furo's own documentation.
+    # "furo.sphinxext",
+    # External stuff
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
+]
+
+
+# templates_path = ['_templates']
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+
+html_static_path = ["_static"]
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+---
+hide-toc: true
+---
+
+# TRAO User Guides
+
+The Taeduk Radio Astronomy Observatory (TRAO) is a 13.7-meter millimeter single-dish radio telescope operated by the Korea Astronomy and Space Science Institute (KASI). Utilizing the 16-beam array SEQUOIA-TRAO receiver, TRAO can perform dual-channel mapping observations in the frequency range of 85-115 GHz with a velocity resolution of 0.05 km/s. TRAO is an observation facility capable of rapidly and efficiently mapping observation targets covering an area of several square degrees.
+
+These documents provide guidance for TRAO users regarding observation, scientific research, proposal submission, data processing, and analysis to assist with TRAO observations. For fundamental information about TRAO such as official notices, status reports, schedules, proposal submission, publication lists, and science program lists, please refer to the official TRAO website (https://trao.kasi.re.kr).
